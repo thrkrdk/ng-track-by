924044252ca1a4c3b5f4f707e2ff7d9026d45f07
thrkardak <thrkardak@gmail.com>
2019-01-27 22:46:20 +0300

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-track-by';
+  erkekOgrenciler = [
+    {id: 1, adi: 'Ali', not: Math.floor(Math.random() * 10)}, {id: 2, adi: 'Veli', not: Math.floor(Math.random() * 10)},
+    {id: 3, adi: 'Ahmet', not: Math.floor(Math.random() * 10)}
+  ];
+
+  kizOgrenciler = [
+    {id: 1, adi: 'Ayşe', not: Math.floor(Math.random() * 10)}, {id: 2, adi: 'Fatma', not: Math.floor(Math.random() * 10)},
+    {id: 3, adi: 'Hatice', not: Math.floor(Math.random() * 10)}];
+
+  getErkekOgrenciler(): void {
+    this.erkekOgrenciler = [
+      {id: 1, adi: 'Ali', not: Math.floor(Math.random() * 10)}, {id: 2, adi: 'Veli', not: Math.floor(Math.random() * 10)},
+      {id: 3, adi: 'Ahmet', not: Math.floor(Math.random() * 10)}, {id: 4, adi: 'mehmet', not: Math.floor(Math.random() * 10)}];
+  }
+
+  getKizOgrenciler(): void {
+    this.kizOgrenciler = [
+      {id: 1, adi: 'Ayşe', not: Math.floor(Math.random() * 10)}, {id: 2, adi: 'Fatma', not: Math.floor(Math.random() * 10)},
+      {id: 3, adi: 'Hatice', not: Math.floor(Math.random() * 10)}, {id: 4, adi: 'Kezban', not: Math.floor(Math.random() * 10)}];
+  }
+
+  trackByFn(index: number, kisi): number {
+    return kisi.id;
+  }
+
+
 }
